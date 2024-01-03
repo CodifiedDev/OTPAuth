224acacd77ad08c2e5f061be9ee967eca67b927a
Ben <87502947+CodifiedDev@users.noreply.github.com>
2024-01-03 10:42:54 +1000

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,5 @@
+﻿// See https://aka.ms/new-console-template for more information
+using OTPAuth.src;
+
+Console.WriteLine("Hello, World!");
+HMACGeneration.generateBase32Key();
---
+++ src/HMACGeneration.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using SimpleBase;
+using System.Security.Cryptography;
+
+
+namespace OTPAuth.src
+{
+    internal class HMACGeneration
+    {
+        public static string generateBase32Key() {
+            //Random random = new Random();
+            //int rand = random.Next(100000000, 999999999); // This method is not cryptographically secure, however it does generate valid 
+            string rand = RandomNumberGenerator.GetHexString(128, true);
+            Console.WriteLine(rand);
+            string base32 = SimpleBase.Base32.Rfc4648.Encode(Encoding.UTF8.GetBytes(rand));
+            Console.WriteLine(base32);
+            return "Test!";
+        }
+    }
+}
